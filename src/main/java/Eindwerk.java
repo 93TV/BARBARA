aa52aed4146a0ba17bf42fc97f37047e0e3f21b4
Thibault Viaene <thibault.viaene@student.odisee.be>
2023-09-28 09:50:50 +0200

    student en eindwerk klasse toegevoegd

--- src/main/java/Eindwerk.java
+++ src/main/java/Eindwerk.java
@@ -5,4 +5,47 @@
  * @version 28/09/2023
  */
 public class Eindwerk {
+    private String titel;
+    private int jaartal;
+    private String opleiding;
+    private Student student;
+
+    public Eindwerk(String titel, int jaartal, String opleiding, Student student) {
+        this.titel = titel;
+        this.jaartal = jaartal;
+        this.opleiding = opleiding;
+        this.student = student;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public int getJaartal() {
+        return jaartal;
+    }
+
+    public void setJaartal(int jaartal) {
+        this.jaartal = jaartal;
+    }
+
+    public String getOpleiding() {
+        return opleiding;
+    }
+
+    public void setOpleiding(String opleiding) {
+        this.opleiding = opleiding;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -3,4 +3,37 @@
  * @author thibe
  * @version 28/09/2023
  */public class Student {
+     private String voornaam;
+     private String familienaam;
+     private String studentennummer;
+
+    public Student(String voornaam, String familienaam, String studentennummer) {
+        this.voornaam = voornaam;
+        this.familienaam = familienaam;
+        this.studentennummer = studentennummer;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public String getFamilienaam() {
+        return familienaam;
+    }
+
+    public void setFamilienaam(String familienaam) {
+        this.familienaam = familienaam;
+    }
+
+    public String getStudentennummer() {
+        return studentennummer;
+    }
+
+    public void setStudentennummer(String studentennummer) {
+        this.studentennummer = studentennummer;
+    }
 }
