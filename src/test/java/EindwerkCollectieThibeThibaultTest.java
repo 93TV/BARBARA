d68f44ed9b0dd9d1ccd7a9529c35ee8cde2c9da3
tuprec <thibe.provost@student.odisee.com>
2023-10-05 09:24:17 +0200

    testen map

---
+++ src/test/java/EindwerkCollectieThibeThibaultTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * BARBARA:EindwerkCollectieThibeThibaultTest
+ *
+ * @author thibe
+ * @version 5/10/2023
+ */
+class EindwerkCollectieThibeThibaultTest {
+
+}
