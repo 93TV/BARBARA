9265d719deb298b55efb53ecc75e3357db131d20
tuprec <thibe.provost@student.odisee.com>
2023-10-05 09:28:23 +0200

    test student

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * BARBARA:StudentTest
+ *
+ * @author thibe
+ * @version 5/10/2023
+ */
+class StudentTest {
+    @Test
+    void uniqueStudentID() {
+    assertAll("student id aanmaken",
+            assertSame(););
+    }
+}
